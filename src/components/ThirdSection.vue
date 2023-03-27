149e80653b59f6fb046bbf2ff75ad7e23f891c36
Goswami_labs <89575497+garimagiri@users.noreply.github.com>
2023-03-27 20:44:30 +0530

    scroll design

--- src/components/ThirdSection.vue
+++ src/components/ThirdSection.vue
@@ -1,9 +1,8 @@
 <template>
   <div @scroll="handleScroll">
-    <div class="position-relative background-css">
+    <div class="position-relative background-css" v-if="isVisible">
       <div
         class="position-absolute top-50 end-0 translate-middle-y bg-white p-3 me-5 protection-box"
-        v-if="isVisible"
       >
         <div class="fs-9">
           <span class="badge bg-danger me-2">New</span
@@ -43,8 +42,11 @@
           </button>
         </div>
       </div>
-
-      <div v-else class="position-absolute top-50 end-0 translate-middle-y bg-white p-3 me-5 protection-box">
+    </div>
+    <div class="position-relative background-css2" v-else>
+      <div
+        class="position-absolute top-50 end-0 translate-middle-y p-2 ps-3 me-5 protection-box2"
+      >
         <div class="mb-2 fs-5 fw-bolder">All-in-one protection</div>
         <p class="fs-8">
           Online protection for your identity, privacy, and devices, includes
@@ -59,7 +61,7 @@
         </div>
         <div class="d-flex my-3 align-items-center">
           <button
-            class="btn btn-danger fs-8 me-4"
+            class="btn btn-danger fs-8 me-2"
             style="border-radius: 40px"
             type="submit"
           >
@@ -75,9 +77,7 @@
         </div>
       </div>
     </div>
-    <div style="height: 50Vh;"></div>
-      
-    
+    <div style="height: 50vh"></div>
   </div>
 </template> 
 <script>
@@ -93,7 +93,7 @@ export default {
   },
   methods: {
     handleScroll() {
-      if (window.scrollY > 900) {
+      if (window.scrollY > 850) {
         this.isVisible = false;
       } else {
         this.isVisible = true;
