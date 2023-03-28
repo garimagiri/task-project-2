3ce063aab7bbd5fd3a5a3de0f82fe0dd14b8fd93
Goswami_labs <89575497+garimagiri@users.noreply.github.com>
2023-03-28 15:33:13 +0530

    tab select

--- src/components/IndividualTab.vue
+++ src/components/IndividualTab.vue
@@ -1,77 +1,68 @@
 <template>
-    <div class="px-3 py-2 pricing-box mb-4 me-5">
-                <div
-                  class="row d-flex justify-content-center align-items-center"
-                >
-                  <div class="col-sm-8">
-                    <span class="fs-8 me-2">Advanced</span
-                    ><span class="badge rounded-pill bg-danger text-uppercase"
-                      >Best value</span
-                    >
-                  </div>
-                  <div class="col-sm-4">
-                    <div>
-                      <span class="fw-bold me-1">$84.99</span>
-                      <span class="fs-9 text-secondary">/yr*</span>
-                    </div>
-                    <div class="fs-9 fw-bold">
-                      <span
-                        class="text-decoration-line-through text-secondary me-2"
-                        >$199.99</span
-                      ><span class="text-danger">Save $115.00</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="px-3 py-2 pricing-box mb-4 me-5">
-                <div
-                  class="row d-flex justify-content-center align-items-center"
-                >
-                  <div class="col-sm-8 fs-8">
-                    <span>Ultimate</span>
-                  </div>
-                  <div class="col-sm-4">
-                    <div>
-                      <span class="fw-bold me-1">$189.99</span>
-                      <span class="fs-9 text-secondary">/yr*</span>
-                    </div>
-                    <div class="fs-9 fw-bold">
-                      <span
-                        class="text-decoration-line-through text-secondary me-2"
-                        >$199.99</span
-                      ><span class="text-danger">Save $90.00</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  <div class="px-3 py-2 pricing-box mb-4 me-5">
+    <div class="row d-flex justify-content-center align-items-center">
+      <div class="col-sm-8">
+        <span class="fs-8 me-2">Advanced</span
+        ><span class="badge rounded-pill bg-danger text-uppercase"
+          >Best value</span
+        >
+      </div>
+      <div class="col-sm-4">
+        <div>
+          <span class="fw-bold me-1">$84.99</span>
+          <span class="fs-9 text-secondary">/yr*</span>
+        </div>
+        <div class="fs-9 fw-bold">
+          <span class="text-decoration-line-through text-secondary me-2"
+            >$199.99</span
+          ><span class="text-danger">Save $115.00</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="px-3 py-2 pricing-box mb-4 me-5">
+    <div class="row d-flex justify-content-center align-items-center">
+      <div class="col-sm-8 fs-8">
+        <span>Ultimate</span>
+      </div>
+      <div class="col-sm-4">
+        <div>
+          <span class="fw-bold me-1">$189.99</span>
+          <span class="fs-9 text-secondary">/yr*</span>
+        </div>
+        <div class="fs-9 fw-bold">
+          <span class="text-decoration-line-through text-secondary me-2"
+            >$199.99</span
+          ><span class="text-danger">Save $90.00</span>
+        </div>
+      </div>
+    </div>
+  </div>
 
-              <div class="row fs-8">
-                <p class="col-sm-5">&#10003;Identity Monitoring</p>
-                <p class="col-sm-7">
-                  &#10003;<span class="text-danger">NEW</span> Personal Data
-                  Cleanup (full-services)
-                </p>
-              </div>
-              <div class="row fs-8">
-                <p class="col-sm-5">&#10003;Unlimited VPN</p>
-                <p class="col-sm-7">
-                  &#10003;<span class="text-danger">NEW</span> Credit Monitoring
-                  (1 bureau)
-                </p>
-              </div>
-              <div class="row fs-8">
-                <p class="col-sm-5">&#10003;Protection</p>
-                <p class="col-sm-7">
-                  &#10003;<span class="text-danger">NEW</span> credit Score &
-                  Report (monthly)
-                </p>
-              </div>
-              <div class="row fs-8">
-                <p class="col-sm-5">&#10003;Transaction Monitoring</p>
-                <p class="col-sm-7">
-                  &#10003;$1M ID theft coverage &restoration
-                </p>
-              </div>
+  <div class="row fs-8">
+    <p class="col-sm-5">&#10003;Identity Monitoring</p>
+    <p class="col-sm-7">
+      &#10003;<span class="text-danger">NEW</span> Personal Data Cleanup
+      (full-services)
+    </p>
+  </div>
+  <div class="row fs-8">
+    <p class="col-sm-5">&#10003;Unlimited VPN</p>
+    <p class="col-sm-7">
+      &#10003;<span class="text-danger">NEW</span> Credit Monitoring (1 bureau)
+    </p>
+  </div>
+  <div class="row fs-8">
+    <p class="col-sm-5">&#10003;Protection</p>
+    <p class="col-sm-7">
+      &#10003;<span class="text-danger">NEW</span> credit Score & Report
+      (monthly)
+    </p>
+  </div>
+  <div class="row fs-8">
+    <p class="col-sm-5">&#10003;Transaction Monitoring</p>
+    <p class="col-sm-7">&#10003;$1M ID theft coverage &restoration</p>
+  </div>
 </template>
 <script>
 export default {
